3b7460356c4e4675b18a5e0e4fdf50a7d9f47b06
komatsu <komatsu.tendo1130@gmail.com>
2021-10-30 22:28:37 +0900

    [add] WebMvcConfig

---
+++ VocabularyCard/src/main/java/com/example/vocabularycard/config/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package com.example.vocabularycard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+
+@Configuration
+@EnableAsync
+public class WebMvcConfig{
+
+}
